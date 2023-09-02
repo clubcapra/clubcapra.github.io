9f4abfec528ea397b468a4db6304efa45d835d8d
Samuel Lachance <leuchak@slachance.ca>
2023-09-02 17:07:34 -0400

    force i18n to use the CompositionApi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { createApp } from 'vue';
 import router from './router';
 import store from './store';
+import i18n from './plugins/i18n';
 
 // Load Vuetify
 import vuetify from './plugins/vuetify';
@@ -14,6 +15,7 @@ const vue = createApp(App);
 vue.use(router);
 vue.use(store);
 vue.use(vuetify);
+vue.use(i18n);
 
 // Run!
 router.isReady().then(() => {
