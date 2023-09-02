0007ab083ee3815556b71094b7807dedf6bb6e80
Samuel Lachance <leuchak@slachance.ca>
2023-09-02 16:46:00 -0400

    config eslint ignore path

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Components
+
+import NavbarComponent from './components/NavbarComponent.vue';
+
+// Assets
+import favicon16x16 from './assets/favicon-16x16.png';
+import favicon32x32 from './assets/favicon-32x32.png';
+import appleTouchIcon from './assets/apple-touch-icon.png';
+import safaryPinnedTab from './assets/safari-pinned-tab.svg';
+
+// Metadata
+</script>
 
 <template>
-  <div>hello</div>
+  <NavbarComponent />
+  <teleport to="head">
+    <link
+      rel="apple-touch-icon"
+      :href="appleTouchIcon"
+      type="image/png"
+      size="152x152"
+    />
+    <link rel="icon" :href="favicon16x16" type="image/png" sizes="16x16" />
+    <link rel="icon" :href="favicon32x32" type="image/png" sizes="32x32" />
+    <link
+      rel="mask-icon"
+      :href="safaryPinnedTab"
+      type="image/svg+xml"
+      color="#5bbad5"
+    />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+  </teleport>
 </template>
