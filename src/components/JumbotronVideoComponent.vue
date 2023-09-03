5fd0e3b86dca4b7d4ff0d28f682580a0a5d62336
Samuel Lachance <leuchak@slachance.ca>
2023-09-03 12:41:05 -0400

    create the JumbotronVideoComponent

---
+++ src/components/JumbotronVideoComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  video?: string;
+  single?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="jumbotron d-flex align-items-center">
+    <h1 class="text-h4 mb-4">{{ title }}</h1>
+    <video v-if="video" class="bg-video" preload="true" autoplay muted>
+      <source :src="video" type="video/mp4" />
+    </video>
+  </div>
+</template>
