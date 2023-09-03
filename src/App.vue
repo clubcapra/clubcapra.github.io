3d489cbcc145b2a2727102366ce61b9f048e88b7
Samuel Lachance <leuchak@slachance.ca>
2023-09-03 12:46:24 -0400

    Populate the Home page with the previous sections

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <script setup lang="ts">
-// Components
+import type { WritableComputedRef } from 'vue';
+import { computed, onMounted } from 'vue';
+import { useGlobalStore } from './store';
 
+// Components
 import FooterComponent from './components/FooterComponent.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
 
-// Assets
-import favicon16x16 from './assets/favicon-16x16.png';
-import favicon32x32 from './assets/favicon-32x32.png';
-import appleTouchIcon from './assets/apple-touch-icon.png';
-import safaryPinnedTab from './assets/safari-pinned-tab.svg';
+/** Global Store */
+const globalStore = useGlobalStore();
 
-// Metadata
+/** loading overlay visibility */
+const loading: WritableComputedRef<boolean> = computed({
+  get: () => globalStore.loading,
+  set: v => globalStore.setLoading(v),
+});
+
+onMounted(() => {
+  loading.value = false;
+});
 </script>
 
 <template>
-  <NavbarComponent />
-  <FooterComponent />
-  <teleport to="head">
-    <link
-      rel="apple-touch-icon"
-      :href="appleTouchIcon"
-      type="image/png"
-      size="152x152"
-    />
-    <link rel="icon" :href="favicon16x16" type="image/png" sizes="16x16" />
-    <link rel="icon" :href="favicon32x32" type="image/png" sizes="32x32" />
-    <link
-      rel="mask-icon"
-      :href="safaryPinnedTab"
-      type="image/svg+xml"
-      color="#5bbad5"
-    />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="theme-color" content="#ffffff" />
-  </teleport>
+  <v-app>
+    <NavbarComponent />
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <v-overlay :model-value="loading" app class="justify-center align-center">
+      <v-progress-circular indeterminate size="64" />
+    </v-overlay>
+
+    <FooterComponent />
+  </v-app>
 </template>
