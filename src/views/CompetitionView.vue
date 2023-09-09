4a1b382388d924f3d45dc38d6168f3c46e499d93
Samuel Lachance <leuchak@slachance.ca>
2023-09-09 09:35:17 -0400

    Set img background in the CompetitionView

--- src/views/CompetitionView.vue
+++ src/views/CompetitionView.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
+// Components
 import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoComponent.vue';
+
+// Media
+import team2021 from '@clubcapra/assets/media/team2021_2.jpg';
 </script>
 
 <template>
-  <JumbotronVideoComponent title="Compétitions" />
-  <div
-    class="jumbotron jumbotron-single d-flex align-items-center"
-    style="background-image: url(img/team2021_2.jpg)"
-  >
-    <div class="container text-center">
-      <h1 class="display-2 mb-4">Compétitions</h1>
-    </div>
-  </div>
+  <JumbotronVideoComponent title="Compétitions" :img="team2021" />
   <section id="section-featurettes" class="featurettes">
     <div class="container">
       <div class="section-content">
@@ -40,7 +36,11 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
                 class="col-md-8 offset-md-right-2 col-sm-6"
                 data-aos="flip-right"
               >
-                <img class="my-5" src="img/robotcuprescue.png" alt="" />
+                <img
+                  class="my-5"
+                  src="src/assets/media/robotcuprescue.png"
+                  alt=""
+                />
               </div>
               <!--/ .col-md-4.col-md-offset-right-2.col-sm-6 -->
             </div>
@@ -67,19 +67,19 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
     <div class="row">
       <div class="col-lg-4 mb-4 mb-lg-0">
         <img
-          src="img/bangkok/team_working1.jpg"
+          src="src/assets/media/bangkok/team_working1.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Mountains in the Clouds"
         />
         <img
-          src="img/bangkok/team_working2.jpg"
+          src="src/assets/media/bangkok/team_working2.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Mountains in the Clouds"
         />
       </div>
       <div class="col-lg-8 col-md-12 mb-4 mb-lg-0">
         <img
-          src="img/bangkok/team_2022.jpg"
+          src="src/assets/media/bangkok/team_2022.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Wintry Mountain Landscape"
         />
@@ -88,14 +88,14 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
     <div class="row">
       <div class="col-md-4 mb-4 mb-lg-0">
         <img
-          src="img/bangkok/markhor_dex.jpg"
+          src="src/assets/media/bangkok/markhor_dex.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Boat on Calm Water"
         />
       </div>
       <div class="col-md-8 mb-4 mb-lg-0">
         <img
-          src="img/bangkok/rrl.JPG"
+          src="src/assets/media/bangkok/rrl.JPG"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Boat on Calm Water"
         />
@@ -116,18 +116,18 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
     <div class="row">
       <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
         <img
-          src="img/dortmund/markhor_reparation.jpg"
+          src="src/assets/media/dortmund/markhor_reparation.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor Réparation"
         />
 
         <img
-          src="img/dortmund/markhor_obstacle.jpg"
+          src="src/assets/media/dortmund/markhor_obstacle.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor Obstacle"
         />
         <img
-          src="img/dortmund/markhor_ui.jpg"
+          src="src/assets/media/dortmund/markhor_ui.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor UI"
         />
@@ -135,21 +135,21 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
 
       <div class="col-lg-8 mb-4 mb-lg-0">
         <img
-          src="img/dortmund/team2021.jpg"
+          src="src/assets/media/dortmund/team2021.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Équipe 2021"
         />
         <div class="row">
           <div class="col-md-6">
             <img
-              src="img/dortmund/markhor_assemblage.jpg"
+              src="src/assets/media/dortmund/markhor_assemblage.jpg"
               class="w-100 shadow-1-strong rounded mb-4"
               alt="Markhor Assemblage"
             />
           </div>
           <div class="col-md-6">
             <img
-              src="img/dortmund/markhor_parcours.jpg"
+              src="src/assets/media/dortmund/markhor_parcours.jpg"
               class="w-100 shadow-1-strong rounded mb-4"
               alt="Markhor Parcours"
             />
@@ -172,35 +172,35 @@ import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoCompone
     <div class="row">
       <div class="col-lg-8 mb-4 mb-lg-0">
         <img
-          src="img/sydney/comp_team_2019.JPG"
+          src="src/assets/media/sydney/comp_team_2019.JPG"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Équipe 2021"
         />
         <img
-          src="img/sydney/rc2019_2.jpg"
+          src="src/assets/media/sydney/rc2019_2.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor Assemblage"
         />
       </div>
       <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
         <img
-          src="img/sydney/rc2019_3.jpg"
+          src="src/assets/media/sydney/rc2019_3.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor Réparation"
         />
 
         <img
-          src="img/sydney/takin1.jpg"
+          src="src/assets/media/sydney/takin1.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor Obstacle"
         />
         <img
-          src="img/sydney/takin5.jpg"
+          src="src/assets/media/sydney/takin5.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor UI"
         />
         <img
-          src="img/sydney/takin_assemblage.jpg"
+          src="src/assets/media/sydney/takin_assemblage.jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt="Markhor UI"
         />
