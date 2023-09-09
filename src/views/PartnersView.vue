f6db27514c4723696a3560d91552ddb8add2bc92
Samuel Lachance <leuchak@slachance.ca>
2023-09-09 13:34:07 -0400

    import all partners img in vuejs

--- src/views/PartnersView.vue
+++ src/views/PartnersView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoComponent.vue';
 
+// Documents
 import plantPartenariat from '@clubcapra/assets/documents/CAPRA_PlanPartenariat.pdf';
+
+// Partners logos
+import EtsTypoGrise from '@clubcapra/assets/media/partners/Logo_ETS_TypoGrise_D_EN.png';
+import hsLogoCouleur from '@clubcapra/assets/media/partners/HS_logo_couleur.png';
+import automatech from '@clubcapra/assets/media/partners/automatech.png';
+import ixiasoftMadcapLogoRgbC from '@clubcapra/assets/media/partners/IXIASOFT-Madcap-logo-RGB-C.jpg';
+import cgLogoCir from '@clubcapra/assets/media/partners/CG_LOGO_CIR.png';
+import aeets from '@clubcapra/assets/media/partners/aeets.svg';
+import altium from '@clubcapra/assets/media/partners/altium.svg';
+import goLabo from '@clubcapra/assets/media/partners/go-labo.png';
+import mtt from '@clubcapra/assets/media/partners/mtt.png';
 </script>
 
 <template>
@@ -10,45 +22,45 @@ import plantPartenariat from '@clubcapra/assets/documents/CAPRA_PlanPartenariat.
     <div class="container">
       <div class="section-content">
         <div class="title-wrap partner-title gold-title" data-aos="fade-up">
-          <h2 class="section-title">Or</h2>
+          <h2 class="section-title">{{ $t('partner_gold') }}</h2>
         </div>
         <div class="row justify-content-center align-items-center py-5">
           <div class="col-md-6 col-12">
-            <img src="../img/partners/Logo_ETS_TypoGrise_D_EN.png" />
+            <img :src="EtsTypoGrise" />
           </div>
           <div class="col-md-6 col-12">
-            <img src="../img/partners/HS_logo_couleur.png" />
+            <img :src="hsLogoCouleur" />
           </div>
           <div class="col-md-6 col-12">
-            <img src="../img/partners/automatech.png" />
+            <img :src="automatech" />
           </div>
           <div class="col-md-6 col-12">
-            <img src="../img/partners/IXIASOFT-Madcap-logo-RGB-C.jpg" />
+            <img :src="ixiasoftMadcapLogoRgbC" />
           </div>
           <div class="col-md-6 col-12">
-            <img src="../img/partners/CG_LOGO_CIR.png" />
+            <img :src="cgLogoCir" />
           </div>
         </div>
         <div class="title-wrap partner-title silver-title" data-aos="fade-up">
-          <h2 class="section-title">Argent</h2>
+          <h2 class="section-title">{{ $t('partner_silver') }}</h2>
         </div>
         <div class="row justify-content-center align-items-center py-5">
           <div class="col-md-3 col-6">
-            <img src="../img/partners/aeets.png" />
+            <img :src="aeets" />
           </div>
           <div class="col-md-3 col-6">
-            <img src="../img/partners/altium.svg" />
+            <img :src="altium" />
           </div>
         </div>
         <div class="title-wrap partner-title bronze-title" data-aos="fade-up">
-          <h2 class="section-title">Bronze</h2>
+          <h2 class="section-title">{{ $t('partner_bronze') }}</h2>
         </div>
         <div class="row justify-content-center align-items-center py-5">
           <div class="col-md-2 col-4">
-            <img src="../img/partners/go-labo.png" />
+            <img :src="goLabo" />
           </div>
           <div class="col-md-2 col-4">
-            <img src="../img/partners/mtt.png" />
+            <img :src="mtt" />
           </div>
         </div>
         <div class="col-md-12">
