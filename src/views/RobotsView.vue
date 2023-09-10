8a144dbd9bbf501e4563b429124279b5ec15ad1f
Samuel Lachance <leuchak@slachance.ca>
2023-09-10 11:47:54 -0400

    Create RobotsView.vue

---
+++ src/views/RobotsView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Components
+import JumbotronVideoComponent from '@clubcapra/components/JumbotronVideoComponent.vue';
+
+// Media
+import robotsBG from '@clubcapra/assets/media/robotsbg.png';
+</script>
+
+<template>
+  <JumbotronVideoComponent :title="$t('our_robots')" :img="robotsBG" />
+</template>
