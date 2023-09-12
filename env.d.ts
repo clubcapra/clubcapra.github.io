3d1ebd7deaa15265bd1282450fbe9c5ae0ab815e
Samuel Lachance <leuchak@slachance.ca>
2023-09-12 14:00:32 -0400

    add matomo

--- env.d.ts
+++ env.d.ts
@@ -7,8 +7,20 @@ declare module 'vuetify/lib/util/colors.mjs';
 // Fullpage.js fix.
 declare module 'vue-fullpage.js';
 
+// Matomo fix.
+declare module 'vue-matomo';
+
 // eslint-disable-next-line no-unused-vars
 interface ImportMetaEnv {
   // see https://vitejs.dev/guide/env-and-mode.html#env-files
   // add .env variables.
+  MATOMO_INSTANCE_URL: string;
+  MATOMO_SITE_ID: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+interface Window {
+  _paq?: {
+    push: (args: string[]) => void;
+  };
 }
