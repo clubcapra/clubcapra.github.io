75fec6c3ebd1630e52c3daf49f928b2f2bd3a385
Samuel Lachance <leuchak@slachance.ca>
2023-09-12 21:14:26 -0400

    fix url /parners to /partners

--- src/views/index.ts
+++ src/views/index.ts
@@ -24,7 +24,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('./CompetitionView.vue'),
   },
   {
-    path: '/parners',
+    path: '/partners',
     name: 'partners',
     component: () => import('./PartnersView.vue'),
   },
