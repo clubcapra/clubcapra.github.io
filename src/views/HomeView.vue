c7adcc983a9a2758f10d0af13a44b03d1bccdbcb
GLDuval <glevesqueduval@gmail.com>
2024-04-05 12:36:26 -0400

    Add robot landing page animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,11 +9,30 @@ import teamcaprarobot from '@clubcapra/assets/media/teamcaprarobot.png';
 </script>
 
 <template>
-  <div class="text-7xl h-screen bg-black">
-    <h1 class="align-middle">
-      Building the future of robotics, one robot at a time for the past 25
-      years.
-    </h1>
+  <div
+    class="flex flex-col md:flex-row max-w-6xl mx-auto justify-between h-screen w-screen px-4 gap-4"
+  >
+    <div>
+      <h1
+        class="md:pt-40 z-10 mx-auto text-primary-500 font-medium font-sans text-5xl md:text-7xl"
+      >
+        Building the future of robotics, together.
+      </h1>
+      <h3
+        class="md:pt-10 z-10 mx-auto text-primary-900 font-medium font-sans text-3xl"
+      >
+        We are a student initiative that aims to develop the next generation of
+        search and rescue robots.
+      </h3>
+    </div>
+
+    <iframe
+      class="rounded-full bg-primary-500 h-1/2 self-center"
+      style="border: none"
+      width="100%"
+      height="100%"
+      src="https://rive.app/community/9385-17851-red-robot/embed"
+    />
   </div>
 
   <!-- Missions -->
