067e25039882607013b90cebbf3bd92fc5310e4b
Nathan <nathan.gt.protic@gmail.com>
2026-05-14 17:27:00 -0400

    update des membres

--- src/components/MemberItemComponent.vue
+++ src/components/MemberItemComponent.vue
@@ -12,18 +12,21 @@ defineProps<{
   <div class="mt-4 pt-2">
     <div class="flex flex-col items-center text-center rounded-full p-3 py-4">
       <a v-if="link" :href="link" target="_blank" class="rounded-full">
+        <div class="member-image-parent">
+          <img
+            :src="img"
+            class="rounded-full h-64 w-64 object-cover shadow-md member-image"
+            alt="capra member"
+          />
+        </div>
+      </a>
+      <div v-else class="member-image-parent">
         <img
           :src="img"
-          class="rounded-full h-64 w-64 object-cover shadow-md"
+          class="rounded-full h-64 w-64 object-cover shadow-md member-image"
           alt="capra member"
         />
-      </a>
-      <img
-        v-else
-        :src="img"
-        class="rounded-full h-64 w-64 object-cover shadow-md"
-        alt="capra member"
-      />
+      </div>
       <div class="mt-3">
         <h4 class="text-lg font-medium mb-0">{{ name }}</h4>
         <small>{{ title }}</small>
@@ -34,8 +37,20 @@ defineProps<{
   </div>
 </template>
 
-<style>
+<style scoped>
 .member-title {
   font-size: 18px;
+}
+.member-image-parent {
+  width: 256px;
+  height: 256px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+.member-image {
+  width: 100%;
+  height: auto;
+  display: block;
+  transform: scale(1.2);
 }
 </style>
--- src/data/members.ts
+++ src/data/members.ts
@@ -9,8 +9,15 @@ import emileLacroix from '@clubcapra/assets/media/members/EMILE_LACROIX.jpg';
 import etienneTardif from '@clubcapra/assets/media/members/ETIENNE_TARDIF.jpg';
 import yanniHaddar from '@clubcapra/assets/media/members/YANNI_HADDAR.jpg';
 import julesLachapelle from '@clubcapra/assets/media/members/JULES_LACHAPELLE.jpg';
-import thommasRousseauSenecal from '@clubcapra/assets/media/members/THOMMAS_ROUSSEAU-SENECAL.jpg';
+import thommasRousseauSenecal from '@clubcapra/assets/media/members/THOMAS_ROUSSEAU-SENECAL.jpg';
 import ilianaDeCarufel from '@clubcapra/assets/media/members/ILIANA_DE_CARUFEL.jpg';
+import mayaBoubekri from '@clubcapra/assets/media/members/MAYA_BOUBEKRI.jpg';
+import lolaHornsperger from '@clubcapra/assets/media/members/LOLA_HORNSPERGER.jpg';
+import williamCaouette from '@clubcapra/assets/media/members/WILLIAM_CAOUETTE.jpg';
+import ghanaisMuniandy from '@clubcapra/assets/media/members/GHANAIS_MUNIANDY.jpg';
+import clementDesmaraisJadin from '@clubcapra/assets/media/members/CLEMENT_DESMARAIS-JADIN.jpg';
+import anthonyAfif from '@clubcapra/assets/media/members/ANTHONY_AFIF.jpg';
+import felixTheriault from '@clubcapra/assets/media/members/FELIX_THERIAULT.jpg';
 
 export interface Member {
   name: string;
@@ -39,6 +46,7 @@ const ÉtienneLG: Member = {
 const AntonyA: Member = {
   name: 'Antony Afif',
   program: 'engineering_electrical',
+  img: anthonyAfif
 };
 
 const AntoineM: Member = {
@@ -81,11 +89,6 @@ const EtienneT: Member = {
   link: 'https://www.linkedin.com/in/etienne-tardif-57b164222/',
 };
 
-const AngeOliveDN: Member = {
-  name: 'Ange Olive Dagang Nyappa',
-  program: 'engineering_software',
-};
-
 const YanniH: Member = {
   name: 'Yanni Haddar',
   program: 'engineering_software',
@@ -97,12 +100,6 @@ const ThomasRS: Member = {
   program: 'engineering_gpa',
   img: thommasRousseauSenecal,
   link: 'https://www.linkedin.com/in/thomas-rousseau-sénécal-14723b346/',
-};
-
-const JaniceB: Member = {
-  name: 'Janice Bessard',
-  program: 'engineering_software',
-  link: 'https://www.linkedin.com/in/janice-b-858122331/',
 };
 
 const JulesL: Member = {
@@ -134,12 +131,7 @@ const WilliamT: Member = {
 const ClementDJ: Member = {
   name: 'Clément Desmarais-Jadin',
   program: 'engineering_mechanical',
-};
-
-const RobertoL: Member = {
-  name: 'Roberto Laframboise',
-  program: 'engineering_gpa',
-  link: 'https://www.linkedin.com/in/roberto-laframboise-974b31326/',
+  img: clementDesmaraisJadin,
 };
 
 const GhanaisM: Member = {
@@ -147,22 +139,13 @@ const GhanaisM: Member = {
   program: 'engineering_gpa',
   title: 'title_social_coordinator',
   link: 'https://www.linkedin.com/in/ghanais',
-};
-
-const WilliamC: Member = {
-  name: 'William Caouette',
-  program: 'engineering_software',
+  img: ghanaisMuniandy,
 };
 
 const AlexisGA: Member = {
   name: 'Alexis Gabriel Aubut',
   program: 'engineering_electrical',
   link: 'www.linkedin.com/in/alexis-gabriel-aubut-03b821342',
-};
-
-const MarieJeanneB: Member = {
-  name: 'Marie-Jeanne Bazinet',
-  program: 'engineering_electrical',
 };
 
 const HugoB: Member = {
@@ -174,36 +157,8 @@ const HugoB: Member = {
 const FelixT: Member = {
   name: 'Félix Thériault',
   program: 'engineering_software',
-};
-
-const TommyV: Member = {
-  name: 'Tommy Vallée',
-  program: 'engineering_gpa',
-  link: 'https://www.linkedin.com/in/tommy-vallee-gpa/',
-};
-
-const YoanP: Member = {
-  name: 'Yoan Paradis',
-  program: 'engineering_electrical',
-  link: 'https://www.linkedin.com/in/yoan-paradis-56a657325',
-};
-
-const RodrigoATP: Member = {
-  name: 'Rodrigo Angel Torres Pérez',
-  program: 'engineering_gpa',
-  link: 'https://github.com/RodriTP, https://ca.linkedin.com/in/rodrigo-angel-torres-p%C3%A9rez-8b38332b4',
-};
-
-const BenjaminT: Member = {
-  name: 'Benjamin Theriault',
-  program: 'engineering_software',
-  title: 'title_partnership_manager',
-  link: 'https://github.com/BenjaminT02',
-};
-
-const LouisSimonL: Member = {
-  name: 'Louis-Simon Leclerc',
-  program: 'engineering_software',
+  link: 'https://www.linkedin.com/in/f%C3%A9lix-th%C3%A9riault-849b2b339/',
+  img: felixTheriault,
 };
 
 const AudreyCMN: Member = {
@@ -214,11 +169,34 @@ const AudreyCMN: Member = {
 const SamuelKB: Member = {
   name: 'Samuel Klein-bichain',
   program: 'engineering_software',
+  link: 'https://www.linkedin.com/in/samuel-klein-bichain/',
 };
 
 const MiguelDSR: Member = {
   name: 'Miguel Da Silva Rhodes',
   program: 'engineering_mechanical',
+  link: 'https://www.linkedin.com/in/miguel-da-silva-4305a43b1/',
+};
+
+const LolaH: Member = {
+  name: 'Lola Hornsperger',
+  program: 'engineering_gpa',
+  link: 'https://www.linkedin.com/in/lola-hornsperger-372353298/',
+  img: lolaHornsperger,
+};
+
+const MayaB: Member = {
+  name: 'Maya Boubekri',
+  program: 'engineering_electrical',
+  link: 'https://www.linkedin.com/in/maya-boubekri-guezout/',
+  img: mayaBoubekri,
+};
+
+const WilliamC: Member = {
+  name: 'Wiliam Caouette',
+  program: 'engineering_software',
+  link: 'https://www.linkedin.com/in/william-caouette-979725185/',
+  img: williamCaouette,
 };
 
 export default {
@@ -231,28 +209,21 @@ export default {
   GuyPhilippeN,
   DenisT,
   EtienneT,
-  AngeOliveDN,
   YanniH,
   ThomasRS,
-  JaniceB,
   JulesL,
   ZackaryB,
   IlianaDC,
   WilliamT,
   ClementDJ,
-  RobertoL,
   GhanaisM,
   WilliamC,
   AlexisGA,
-  MarieJeanneB,
   HugoB,
   FelixT,
-  TommyV,
-  YoanP,
-  RodrigoATP,
-  BenjaminT,
-  LouisSimonL,
   AudreyCMN,
   SamuelKB,
   MiguelDSR,
+  LolaH,
+  MayaB,
 };
